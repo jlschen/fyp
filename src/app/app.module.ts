00cc486491ac7ddecc8c77840bc20cdbd0dc0e31
Jerry Lu <jerrylu@Jerry.local>
2017-09-13 00:45:26 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,24 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AppPage } from '../pages/app/app';
+import { App2Page } from '../pages/app2/app2';
+import { ChinesePage } from '../pages/chinese/chinese';
+import { Chinese2Page } from '../pages/chinese2/chinese2';
+import { MalayPage } from '../pages/malay/malay';
+import { Malay2Page } from '../pages/malay2/malay2';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AppPage,
+    App2Page,
+    ChinesePage,
+    Chinese2Page,
+    MalayPage,
+    Malay2Page
   ],
   imports: [
     BrowserModule,
@@ -19,7 +32,14 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AppPage,
+    App2Page,
+    ChinesePage,
+    Chinese2Page,
+    MalayPage,
+    Malay2Page
+
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { AppPage } from '../app/app';
+import { App2Page } from '../app2/app2';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	appOnePage = AppPage;
+	appTwoPage = App2Page;
 
+  constructor() {
+  	
   }
+
 
 }
